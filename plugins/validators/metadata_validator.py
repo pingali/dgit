1668a572ab956c2fb9ce651c9f8918d9f2b83d2a
Venkata Pingali <pingali@gmail.com>
2016-03-17 14:11:20 +0530

    1. Moved the checksum computation to metadata validator 2. DRY elsewhere

--- plugins/validators/metadata_validator.py
+++ plugins/validators/metadata_validator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python 
 
-import os, sys
+import os, sys, glob2 
 from dgitcore.plugins.validator import ValidatorBase
 from dgitcore.config import get_config 
-from dgitcore.helper import compute_sha256 
+from dgitcore.helper import compute_sha256, cd 
 
 class MetadataValidator(ValidatorBase):     
     """
@@ -43,16 +43,48 @@ class MetadataValidator(ValidatorBase):
         """
         Evaluate the files identified for checksum. 
         """
-        print("Evaluating", files, rules)
-        status = []
-        for f in files: 
-            status.append({
-                'file': f,
-                'status': 'OK',
-                'message': ""
-            })
-        return status 
 
+        status = []
+        with cd(repo.rootdir): 
+            resource_files = repo.find_matching_files("*")
+            files = glob2.glob("**/*")
+            disk_files = [f for f in files if os.path.isfile(f) and f != "datapackage.json"]
+            
+            allfiles = list(set(resource_files + disk_files))
+            allfiles.sort() 
+
+            for f in allfiles: 
+                if f in resource_files and f in disk_files: 
+                    r = repo.get_resource(f)
+                    coded_sha256 = r['sha256']             
+                    computed_sha256 = compute_sha256(f)
+                    if computed_sha256 != coded_sha256: 
+                        status.append({
+                            'target': f,
+                            'status': 'ERROR',
+                            'message': "Mismatch in checksum on disk and in datapackage.json"
+                        })
+                    else: 
+                        status.append({
+                            'target': f,
+                            'status': 'OK',
+                            'message': ""
+                        })
+                elif f in resource_files: 
+                    status.append({
+                            'target': f,
+                            'status': 'ERROR',
+                            'message': "In datapackage.json but not in repo"
+                        })
+                else:
+                    status.append({
+                        'target': f,
+                        'status': 'ERROR',
+                        'message': "In repo but not in datapackage.json"
+                        })
+                    
+
+        return status 
     
 def setup(mgr): 
     
