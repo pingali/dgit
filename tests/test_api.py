e4e4210f4ef2b40b138232f9393431e0cde43b3e
Venkata Pingali <pingali@gmail.com>
2016-03-30 19:58:44 +0530

    1. DRY 2. Bootstrapped cli testing code 3. Cleanup of the checks

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,20 @@
-import os, sys, shutil, tempfile, json 
+import os, sys, shutil, tempfile, json, stat 
 from nose import with_setup
 from nose.tools import assert_raises
 from unittest import TestCase
+
+####################################################
+# This file has (an incomplete) list of tests for the API. In
+# particular it tests whether repos are being created correctly, and
+# when we add files they do show up in all the right places. The list
+# is not complete. This needs more work.
+####################################################
 
 # setup the test environment and then import dgitcore 
 thisdir = os.path.dirname(os.path.abspath(__file__))
 os.environ['DGIT_INI'] = os.path.join(thisdir, 
                                       'assets', 
-                                      'dgit-1.ini')
+                                      'dgit-api.ini')
 from dgitcore.exceptions import * 
 from dgitcore import api 
 from dgitcore.helper import cd 
@@ -22,7 +29,7 @@ def clean_workspace():
     """
     config = api.get_config() 
     workspace = config['Local']['workspace']        
-    default_workspace = os.path.expanduser("~/.dgit.ini")
+    default_workspace = os.path.expanduser("~/.dgit")
     if workspace != default_workspace and os.path.exists(workspace): 
         print("Removing tree", workspace) 
         shutil.rmtree(workspace) 
@@ -36,22 +43,19 @@ def workspace_teardown():
     print("teardown")
     clean_workspace() 
 
-def create_sample_repo(username, reponame): 
+def create_sample_repo_nooptions(username, reponame, setup): 
     # Now try with options 
-    repo = api.init(username, reponame,
-                    setup=None,
-                    options={
-                        "title": "Simple",
-                        "description": "Simple dataset"
-                    },
+    repo = api.init(username, 
+                    reponame,
+                    setup=setup,
                     noinput=True
                 )
     return repo 
 
-def create_sample_s3_repo(username, reponame): 
+def create_sample_repo(username, reponame, setup): 
     # Now try with options 
     repo = api.init(username, reponame,
-                    setup='s3+git',
+                    setup=setup,
                     options={
                         "title": "Simple",
                         "description": "Simple dataset"
@@ -71,19 +75,44 @@ def get_remote_path(result):
 repo_configurations = { 
     'simple1': ['test1', 'testrepo1', None],
     'simple2': ['test2', 'testrepo2', None],
-    's3': ['test_s3', 'testrepo_s3', 's3+git']
+    's3': ['test_s3', 'testrepo_s3', 'git+s3']
 }
 
+def basic_repo_lookup(name): 
+    repo = api.lookup(repo_configurations[name][0],
+                      repo_configurations[name][1])
+    assert repo is not None 
+    return repo 
+    
+def basic_result_check(result): 
+    assert isinstance(result, dict) 
+    assert result['status'] == 'success' 
     
 ###########################################
 # Group 1
 # Simple checks
 ###########################################
-@with_setup(workspace_setup, workspace_teardown)
+@with_setup(workspace_setup,None)
+def test_start_group0():
+    """
+    [Group0] Begin
+    """
+    pass 
+
 def test_missing_repo():
+    """
+    Lookup repo
+    """
     with assert_raises(UnknownRepository):
         api.lookup(repo_configurations['simple1'][0],
                    repo_configurations['simple1'][1])
+
+@with_setup(workspace_setup,None)
+def test_end_group0():
+    """
+    [Group0] End 
+    """
+    pass 
 
 ###########################################
 # Group 2 
@@ -92,28 +121,24 @@ def test_missing_repo():
 @with_setup(workspace_setup,None)
 def test_start_group1():
     """
-    Start group1 
+    [Group1] Begin
     """
     pass 
 
 
 def test_create_repo():
     """
-    Create a repo
+    Init repo
     """
 
     for name in repo_configurations: 
         
         #  This should fail
         with assert_raises(IncompleteParameters):
-            repo = api.init(repo_configurations[name][0],
-                            repo_configurations[name][1],
-                            setup=repo_configurations[name][2],
-                            noinput=True)
+            repo = create_sample_repo_nooptions(*repo_configurations[name])
 
         #  This should be successful 
-        repo = create_sample_repo(repo_configurations[name][0],
-                                  repo_configurations[name][1])
+        repo = create_sample_repo(*repo_configurations[name])
         assert repo is not None 
     
         # Check if all the files have been created 
@@ -127,13 +152,10 @@ def test_create_repo():
 
 def test_sh_command():
     """
-    Test whether sh commands work
+    Shell cmds
     """
 
-    repo = api.lookup(repo_configurations['simple1'][0],
-                      repo_configurations['simple1'][1])
-                      
-    assert repo is not None
+    repo = basic_repo_lookup('simple1')
 
     output = api.shellcmd(repo, ['ls'])
     assert "datapackage.json" in output
@@ -141,49 +163,67 @@ def test_sh_command():
 
 def test_remote(): 
     """
-    Repo backend 
+    S3 hook configuration
     """
-
-    repo = api.lookup(repo_configurations['s3'][0],
-                      repo_configurations['s3'][1])
-    assert repo is not None 
+    repo = basic_repo_lookup('s3')
     
     result = api.remote(repo,['-v'])
-    assert result['status'] == 'success' 
+    basic_result_check(result) 
     assert result['message'] is not None 
 
+    # Basic check...
     backend_repodir = get_remote_path(result) 
     assert os.path.exists(backend_repodir) 
-    
-    
-    
-    
 
+    # Is the post-receive hook there? 
+    path = os.path.join(backend_repodir, 'hooks', 'post-receive')
+    assert os.path.exists(path)
+
+    # Make sure that the post-receive hook has execute permissions 
+    st = os.stat(path)
+    assert bool(st.st_mode & stat.S_IXUSR)
+    
 def test_check_package():
     """
-    Check datapackage.json 
+    Metadata validity
     """
-    repo = api.lookup(repo_configurations['simple1'][0],
-                      repo_configurations['simple1'][1])
-
-    assert repo is not None 
+    repo = basic_repo_lookup('simple1')
         
     rootdir = repo.rootdir
     path = os.path.join(rootdir, 'datapackage.json')    
-    package = json.loads(open(path).read())
-
+    
+    # Can the metadata be loaded to begin with? 
+    try: 
+        package = json.loads(open(path).read())
+        valid = True
+    except:
+        valid = False 
+    assert valid 
+    
 def test_repo_drop():
     """
     Drop repo 
     """
-    repo = api.lookup(repo_configurations['simple1'][0],
-                      repo_configurations['simple1'][1])
-    assert repo is not None 
+
+    repos = api.list_repos()
+    print(repos)
+    assert len(repos) == 3    
+
+    repo = basic_repo_lookup('simple1')
+    rootdir = repo.rootdir 
+    api.drop(repo) 
+    
+    assert not os.path.exists(rootdir)
+
+    repos = api.list_repos()
+    print(repos)
+    assert len(repos) == 2  
+
 
 @with_setup(None, workspace_teardown)
 def test_end_group1():
     """
-    End group1 
+    [Group1] End
     """
     pass 
 
@@ -193,43 +233,55 @@ def test_end_group1():
 @with_setup(workspace_setup,None)
 def test_start_group2():
     """
-    Start group2 
+    [Group2] Begin
     """
     pass 
 
-def test_listing():
-    """
-    List repos
-    """
-    repo1 = create_sample_repo(repo_configurations['simple1'][0],
-                               repo_configurations['simple1'][1])
-    print(repo1)
-    repos = api.list_repos()
-    assert len(repos) == 1
-    
-    repo2 = create_sample_repo(repo_configurations['simple2'][0],
-                               repo_configurations['simple2'][1])
-    repos = api.list_repos()
-    assert len(repos) == 2 
     
 def test_simple_add_files():
     """
-    Check simple file addition
+    Add simple files
     """    
-    repo = api.lookup('test1', 'testrepo1') 
-    assert repo is not None 
-    
-    rootdir = repo.rootdir 
+
+    repo = create_sample_repo(*repo_configurations['simple1'])
         
     # Create a temp file and add it 
     (fd, filename) = tempfile.mkstemp()
     try: 
         api.add(repo, [filename], ".")
+        
+        # Check if the file exists in git 
+        rootdir = repo.rootdir     
         basename = os.path.basename(filename)
         path = os.path.join(rootdir, basename) 
-        print(path) 
-        print(filename)
         assert os.path.exists(path)
+        
+        # Check if the file shows up in the datapackage 
+        package = json.loads(open(os.path.join(rootdir, 'datapackage.json')).read())
+        filenames = [r['relativepath'] for r in package['resources']]
+        assert basename in filenames 
+        
+        # TODO 
+        # Check SHA checksum...
+        
+        # Check if it is showing up in the status command. 
+        result = api.status(repo) 
+        basic_result_check(result) 
+        assert "new file" in result['message'] 
+
+        # Check if it is showing up in the status command. 
+        result = api.commit(repo, ["-a", "-m", "Test file addition"])
+        basic_result_check(result) 
+
+        result = api.status(repo) 
+        basic_result_check(result) 
+        assert "new file" not in result['message'] 
+
+        # Check whether the commit message shows the commit message 
+        result = api.log(repo) 
+        basic_result_check(result) 
+        assert "Test file addition" in result['message'] 
+        
     except Exception as e:
         # Cleanup and throw the exception again 
         os.unlink(filename) 
@@ -237,23 +289,11 @@ def test_simple_add_files():
 
     # Cleanup 
     os.unlink(filename) 
-
-def test_status():
-    """
-    Status command
-    """    
-
-    repo = api.lookup(repo_configurations['simple1'][0],
-                      repo_configurations['simple1'][1])
-    assert repo is not None 
-
-    result = api.status(repo) 
-    assert isinstance(result, dict) 
-    assert result['status'] == 'success' 
+    
 
 @with_setup(None, workspace_teardown)
 def test_end_group2():
     """
-    End group2 
+    [Group2] End
     """
     pass 
---
+++ tests/test_cli.py
@@ -0,0 +1,81 @@
+import os, sys, shutil, tempfile, json, stat 
+from nose import with_setup
+from nose.tools import assert_raises
+from unittest import TestCase
+from click.testing import CliRunner
+import imp
+
+runner = CliRunner()
+
+####################################################
+# This file has (an incomplete) list of tests for the command line. In
+# particular it tests whether repos are being created correctly, and
+# when we add files they do show up in all the right places. The list
+# is not complete. This needs more work.
+#####################################################
+
+##########################################################
+# Load the command line and set the config file..
+##########################################################
+thisdir = os.path.abspath(os.path.dirname(__file__))
+os.environ['DGIT_INI'] = os.path.join(thisdir, 
+                                      'assets', 
+                                      'dgit-cli.ini')
+
+# Load the dgit command file...
+dgitfile = os.path.join(thisdir, "..", "bin", "dgit")
+dgitmod = imp.load_source('dgit', dgitfile)
+
+##########################################################
+# Setup and clean workspace 
+##########################################################
+def clean_workspace(): 
+    """
+    Clean the working space 
+    """    
+    workspace = os.path.join(thisdir, 'workspace') 
+    default_workspace = os.path.expanduser("~/.dgit")
+    if ((workspace != default_workspace) and 
+        os.path.exists(workspace)): 
+        print("Removing tree", workspace) 
+        shutil.rmtree(workspace) 
+    
+def workspace_setup():     
+    print("Setup")
+    dgitmod.setup() 
+    clean_workspace() 
+
+def workspace_teardown(): 
+    print("teardown")
+    clean_workspace() 
+    dgitmod.teardown()
+
+repo_configurations = { 
+    'simple1': ['test1', 'testrepo1', None],
+    'simple2': ['test2', 'testrepo2', None],
+    's3': ['test_s3', 'testrepo_s3', 'git+s3']
+}
+
+@with_setup(workspace_setup, workspace_teardown) 
+def test_list():
+    """
+    List repos
+    """
+    result = runner.invoke(dgitmod.list_repos)
+    assert "Found 0 repos" in result.output 
+
+
+@with_setup(workspace_setup, workspace_teardown) 
+def test_init():
+    """
+    Init repo
+    """
+    result = runner.invoke(dgitmod.init, 
+                           [ "{}/{}".format(repo_configurations['simple1'][0],
+                                            repo_configurations['simple1'][1]),
+                             '--setup', 'git',
+                         ],
+                           input="Hello\ntest")
+
+    result = runner.invoke(dgitmod.list_repos)
+    assert "Found 1 repos" in result.output 
